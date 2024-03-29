e3cb8ab1034c0b103acf663f77b378b5a3d5218b
yago-25 <yagofontanez2000@gmail.com>
2024-03-29 17:55:26 -0300

    Adicionando arquivo JS do cabecalho e alterações no css

---
+++ js/cabecalho.js
@@ -0,0 +1,96 @@
+const principalMenuPesquisa = document.querySelector('.pesquisa-menu');
+const menuServicos = document.querySelector('.servicos-menu');
+const menuReservas = document.querySelector('.reservas-menu');
+const menuRenovacoes = document.querySelector('.renovacoes-menu');
+const menuAcervos = document.querySelector('.acervo-menu');
+const menuDownload = document.querySelector('.download-menu');
+const menuNovidades = document.querySelector(".novidades-menu");
+const menuFaleConosco = document.querySelector('.faleconosco-menu');
+
+principalMenuPesquisa.addEventListener('click', function() {
+    principalMenuPesquisa.style.color = 'var(--verde-claro-dois)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuServicos.addEventListener('click', function() {
+    menuServicos.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuReservas.addEventListener('click', function() {
+    menuReservas.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuRenovacoes.addEventListener('click', function() {
+    menuRenovacoes.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuAcervos.addEventListener('click', function() {
+    menuAcervos.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuDownload.addEventListener('click', function() {
+    menuDownload.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuNovidades.addEventListener('click', function() {
+    menuNovidades.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuFaleConosco.style.color = 'var(--branco-ite)';
+});
+
+menuFaleConosco.addEventListener('click', function() {
+    menuFaleConosco.style.color = 'var(--verde-claro-dois)';
+    principalMenuPesquisa.style.color = 'var(--branco-ite)';
+    menuServicos.style.color = 'var(--branco-ite)';
+    menuReservas.style.color = 'var(--branco-ite)';
+    menuRenovacoes.style.color = 'var(--branco-ite)';
+    menuAcervos.style.color = 'var(--branco-ite)';
+    menuDownload.style.color = 'var(--branco-ite)';
+    menuNovidades.style.color = 'var(--branco-ite)';
+});
